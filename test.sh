2e3970902c8cf430315b55da9ccd9228e1597a16
Luke Schoen <ltfschoen@gmail.com>
2018-08-01 09:59:58 +0200

    Add nvchecker progress/issue and failover task

--- test.sh
+++ test.sh
@@ -10,6 +10,18 @@ rsync -avz "/Users/Ls/Library/Application Support/Polkadot/chains/krummelanke/db
 #   - https://www.linode.com/docs/tools-reference/tools/introduction-to-rsync/
 rsync -az --verbose --progress --stats --exclude='.git/' ~/code/linode root@172.104.142.185:/root;
 
+# Watch for latest releases published to a repo 
+# https://github.com/lilydjwg/nvchecker
+git clone https://github.com/lilydjwg/nvchecker;
+cd nvchecker;
+python setup.py install
+
+# Issue created https://github.com/lilydjwg/nvchecker/issues/78
+
+# Create failover with script on the backup node that watches one node for activity and, if it looks dead, issues a killall -9 on it and continues itself.
+
+Otherwise they’ll both be sending BFT messages and you’ll end up double-signing and get slashed.
+
 # Docker Container
 cd linode;
 
